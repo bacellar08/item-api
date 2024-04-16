f4a6fe73589c417a6debf85a2c3871b15d2d5e55
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2024-04-16 20:37:40 -0300

    DELETE Request finished

--- src/main/java/com/itemsapi/itemsapi/controller/ItemController.java
+++ src/main/java/com/itemsapi/itemsapi/controller/ItemController.java
@@ -1,10 +1,15 @@
 package com.itemsapi.itemsapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,4 +44,17 @@ public class ItemController {
         List<ItemResponseDTO> itemList = itemRepository.findAll().stream().map(ItemResponseDTO::new).toList();
         return itemList;
     }
+    
+    @CrossOrigin(origins = "*")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteItem(@PathVariable Long id) {
+        Optional<Item> optionalItem = itemRepository.findById(id);
+        if (optionalItem.isPresent()) {
+            itemRepository.deleteById(id);
+            return ResponseEntity.ok("Item deletado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item não existe");
+        }
+    }
+
 }
